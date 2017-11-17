2c4c16e891d96537a7942ddf64ec69fed4f75b3d
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-11-17 21:34:47 +0800

    Added sprites for land and water

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,11 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody rigidBody;
 
 	//transform data
-	public float xrotation;
-	public float yrotation;
-	public static readonly float deg = 180f / (float)Mathf.PI;
+	private float xrotation;
+	private float yrotation;
+	private static readonly float deg = 180f / (float)Mathf.PI;
+
+	private bool canJump = true;
 
 	void Start() {
 		rigidBody = GetComponent <Rigidbody> ();
@@ -28,12 +30,25 @@ public class PlayerController : MonoBehaviour {
 		transform.RotateAround (transform.position, new Vector3 (1, 0, 0), yrotation * deg);
 		transform.RotateAround (transform.position, new Vector3 (0, 1, 0), xrotation * deg);
 
+		float vy = 0;
+		if (Input.GetButton ("Jump") && canJump) {
+			rigidBody.useGravity = false;
+			vy = 1;
+		} else if (!rigidBody.useGravity) {
+			canJump = false;
+		}
+
 		Vector3 force = Quaternion.Euler (0, xrotation * deg, 0) * new Vector3 (
 			Input.GetAxis ("Horizontal"),
-			0,
+			vy,
 			Input.GetAxis ("Vertical")
 		) * 10;
 		rigidBody.AddForce (force);
+	}
+
+	void OnCollisionEnter(Collision colInfo) {
+		rigidBody.useGravity = true;
+		canJump = true;
 	}
 
 }
